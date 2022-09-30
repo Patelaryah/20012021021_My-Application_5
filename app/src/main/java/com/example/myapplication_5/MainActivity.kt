8d14613373b85618d9f755bf82ddf470347ac595
Patelaryah <patelarya20@gnu.ac.in>
2022-09-30 19:48:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication_5/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.myapplication_5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import androidx.core.view.WindowCompat
+import com.example.myapplication_5.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.topAppbar)
+        val bPlay = findViewById<FloatingActionButton>(R.id.playButton)
+        val bStop = findViewById<FloatingActionButton>(R.id.stopButton)
+        bPlay.setOnClickListener{
+            play()
+        }
+        bStop.setOnClickListener{
+            stop()
+        }
+    }
+    fun play(){
+
+        Intent(applicationContext,MediaService::class.java).putExtra(MediaService.DATA_KEY,MediaService.DATA_VALUE).apply {
+            startService(this)
+        }
+    }
+    fun stop(){
+        Intent(applicationContext,MediaService::class.java).apply {
+            stopService(this)
+        }
+    }
+}
